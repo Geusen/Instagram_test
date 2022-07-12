7f0046c68c976fe62c05d05a22e0c0cb66b3f705
Geusen <89836078+Geusen@users.noreply.github.com>
2022-07-12 13:48:55 +0900

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+CK = os.environ.get("CONSUMER_KEY") # 環境変数の値をCKに代入(以下同様)
+CS = os.environ.get("CONSUMER_SECRET")
+AT = os.environ.get("ACCESS_TOKEN")
+ATC = os.environ.get("ACCESS_TOKEN_SECRET")
+GU = os.environ.get("GOOGLE_URL")
+LN = os.environ.get("LINE_NOTIFY")
+LN27 = os.environ.get("LINE_NOTIFY_27")
+DT = os.environ.get("DISCORD_TOKEN")
+DI = os.environ.get("DISCORD_ID")
+DID = os.environ.get("DISCORD_ID_DEBUG")
+JSON = os.environ.get("JSON")
+CLIENT = os.environ.get("CLIENT")
